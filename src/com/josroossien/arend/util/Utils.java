32a7a084d717b8f55e2e78cae5c1d5b68cfab995
worstboy32 <clashwars@hotmail.com>
2015-01-10 15:33:11 +0100

    %Update to 1.8

--- src/com/josroossien/arend/util/Utils.java
+++ src/com/josroossien/arend/util/Utils.java
@@ -1,11 +1,11 @@
 package com.josroossien.arend.util;
 
-import net.minecraft.server.v1_7_R4.*;
+import net.minecraft.server.v1_8_R1.*;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_7_R4.CraftWorld;
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftEntity;
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftLivingEntity;
-import org.bukkit.craftbukkit.v1_7_R4.entity.CraftProjectile;
+import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftProjectile;
 
 import java.util.Random;
 
@@ -33,7 +33,7 @@ public class Utils {
 		Random random = new Random();
 
 		double d0 = target.locX - loc.getX();
-		double d1 = target.boundingBox.b + target.length / 2.0F - (loc.getY() + target.length / 2.0F);
+		double d1 = target.getBoundingBox().b + target.length / 2.0F - (loc.getY() + target.length / 2.0F);
 		double d2 = target.locZ - loc.getZ();
 		double d3 = (double) MathHelper.sqrt(d0 * d0 + d2 * d2);
 
@@ -51,7 +51,7 @@ public class Utils {
 
 		fireball.setPositionRotation(loc.getX() + d4, loc.getY(), loc.getZ() + d5, f2, f3);
 		fireball.setPosition(fireball.locX, fireball.locY, fireball.locZ);
-		fireball.height = 0.0F;
+		//fireball.height = 0.0F;
 		fireball.motX = fireball.motY = fireball.motZ = 0.0D;
 		// CraftBukkit start - Added setDirection method
 		fireball.setDirection(d0, d1, d2);
